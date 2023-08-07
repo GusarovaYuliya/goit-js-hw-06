4f934784c0664a5d4c61481b760ffc464672fc73
GusarovaYuliya <gusarovayuliya25@gmail.com>
2023-08-08 01:16:28 +0300

    comm

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,11 @@
+// HTML містить порожній список ul#ingredients.
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +14,21 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulElement = document.querySelector('ingredients');
+
+ingredients.
+  ingredients
+ForEach(ingredient => {
+
+  const liElement = document.createElement('li');
+  liElement.
+
+    textContent = ingredient;
+  liElement.
+
+    classList.add('item');
+  ulElement.ForEach
+
+  appendChild(liElement);
+})
